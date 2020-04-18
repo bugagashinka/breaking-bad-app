5d475b3bfc764ac908cffbff779d0ea473a85482
bugagashinka <voloshyn.andrew@gmail.com>
2020-04-18 17:48:08 +0300

    Update Characters page structure (add CharacterItem component)

--- src/components/CharacterItem/CharacterItem.jsx
+++ src/components/CharacterItem/CharacterItem.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import stl from "./CharacterItem.module.scss";
+import { item } from "../List/List.module.scss";
+import classNames from "classnames";
 
-const CharacterItem = () => {
-  return <div></div>;
+const CharacterItem = ({ data: { name, img } }) => {
+  const cardStyles = classNames(`${stl.card}`, `${item}`);
+  return (
+    <div className={cardStyles}>
+      <div className={stl.card__image} style={{ backgroundImage: `url(${img})` }}></div>
+      <h3>{name}</h3>
+    </div>
+  );
 };
 
 export default CharacterItem;
