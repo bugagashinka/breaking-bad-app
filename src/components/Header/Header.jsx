73b9e73a30b3408e51f18a901d30b42fff22c22c
bugagashinka <voloshyn.andrew@gmail.com>
2020-04-18 14:59:16 +0300

    Update Header component styles and structure (also add SearchPanel component)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import stl from "./Header.module.scss";
+import classNames from "classnames";
+import SearchPanel from "components/SearchPanel";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className={classNames("header", `${stl.header}`)}>
+      <section className="container">
+        <div className={stl.inner}>
+          <h1 className={stl.title}>
+            <span className={stl.title__part1}>Breaking</span>
+            <span className={stl.title__part2}>Bad</span>
+          </h1>
+          <nav className={stl.menu}>
+            <ul>
+              <li>
+                <NavLink to="/">Episodes</NavLink>
+              </li>
+              <li>
+                <NavLink to="/characters">Characters</NavLink>
+              </li>
+              <li>
+                <NavLink to="/quotes">Quotes</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <SearchPanel />
+      </section>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-
-const SearchPanel = () => {
-  return <div></div>;
+import stl from "./SearchPanel.module.scss";
+const SearchPanel = ({ placeholder }) => {
+  return (
+    <form className={stl.form}>
+      <input className={stl.field} type="text" placeholder={"Input character name"} />
+      <input className={stl.button} type="submit" value="Submit" />
+    </form>
+  );
 };
 export default SearchPanel;
