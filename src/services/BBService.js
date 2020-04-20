f4759186b446db18874baa4189d35e6c08f4bb8d
bugagashinka <voloshyn.andrew@gmail.com>
2020-04-20 14:28:15 +0300

    Update BBService, extend API

--- src/services/BBService.js
+++ src/services/BBService.js
@@ -1,11 +1,13 @@
 const BASE = "https://www.breakingbadapi.com/api/";
 const CHARACTER_ENDPOINT = "characters/";
+const EPISODE_ENDPOINT = "episodes/";
+const QUOTE_ENDPOINT = "quotes/";
 
 class BBService {
-  async getResource(url) {
-    const res = await fetch(`${BASE}${url}`);
+  async getResource(endpoint) {
+    const res = await fetch(`${BASE}${endpoint}`);
     if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, received ${res.status}`);
+      throw new Error(`Could not fetch ${endpoint}, received ${res.status}`);
     }
 
     return await res.json();
@@ -16,7 +18,23 @@ class BBService {
   }
 
   async getAllCharactes() {
-    return await this.getResource(`${CHARACTER_ENDPOINT}`);
+    return await this.getResource(CHARACTER_ENDPOINT);
+  }
+
+  async getEpisode(id) {
+    return await this.getResource(`${EPISODE_ENDPOINT}${id}`);
+  }
+
+  async getAllEpisodes() {
+    return await this.getResource(EPISODE_ENDPOINT);
+  }
+
+  async getQuote(id) {
+    return await this.getResource(`${QUOTE_ENDPOINT}${id}`);
+  }
+
+  async getAllQuotes() {
+    return await this.getResource(QUOTE_ENDPOINT);
   }
 }
 
