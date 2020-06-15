0ea64f8d7aaf8fe78236c3f31c36f59c4ef9faf6
bugagashinka <voloshyn.andrew@gmail.com>
2020-06-15 12:20:08 +0300

    Add util modules
    -Module that fix inconsistence charaters names
    -Module that make hight order component composition

---
+++ src/utils/fixInconsistenceApi.js
@@ -0,0 +1,31 @@
+const DEFAULT_CATEGORY = "Breaking Bad";
+
+const namesMap = {
+  "Krazy-8": "Domingo Molina",
+  "Hank Schrader": "Henry Schrader",
+  "Ken Winns": "Ken Wins",
+  "Elliott Schwarts": "Elliot Schwartz",
+  "Eliott Schwartz": "Elliot Schwartz",
+  "Gretchen Swartz": "Gretchen Schwartz",
+  Badger: "Brandon Mayhew",
+  "Ted Beneke": "Theodore Beneke",
+  Combo: "Christian Ortgea",
+  "Ken Winns": "Ken Wins",
+  "Mike Erhmantraut": "Mike Ehrmantraut",
+  "Mike Ehrmantraut": "Mike Ehrmantraut",
+  "The cousins": "Marco & Leonel Salamanca",
+  "Steve Gomez": "Steven Gomez",
+};
+
+const checkCategoryRelated = (category, expected = DEFAULT_CATEGORY) => {
+  return category.includes(expected);
+};
+
+const decodeUri = (name) => decodeURIComponent(name).replace(/\+/g, " ");
+
+const getConsistenceName = (name = "") => {
+  const decodedName = decodeUri(name);
+  return namesMap[decodedName] || decodedName;
+};
+
+export { checkCategoryRelated, getConsistenceName };
