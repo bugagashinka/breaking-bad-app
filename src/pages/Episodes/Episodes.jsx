af4385ced4d73cd2cf5468e4cc8da7921ab31904
bugagashinka <voloshyn.andrew@gmail.com>
2020-06-18 14:58:33 +0300

    Add pagination hoc for Episodes

--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import EpisodeItem from "components/EpisodeItem";
 import { BBService } from "services";
 import List from "components/List";
-import { withData, withGeneralLayout } from "hoc";
+import { withData, withGeneralLayout, withEpisodePagination } from "hoc";
 import { compose } from "utils";
 
 const Episodes = ({ data: episodes }) => {
@@ -15,4 +15,4 @@ const Episodes = ({ data: episodes }) => {
   );
 };
 
-export default compose(withData(BBService.getAllEpisodes), withGeneralLayout())(Episodes);
+export default compose(withData(BBService.getAllEpisodes), withGeneralLayout(), withEpisodePagination)(Episodes);
