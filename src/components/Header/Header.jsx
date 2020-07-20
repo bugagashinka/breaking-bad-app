27d191075cfd05b019bb23b6951d06be866198bb
Bugagashinka <voloshyn.andrew@gmail.com>
2020-07-20 17:37:04 +0300

    make menu responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,33 +4,44 @@ import stl from "./Header.module.scss";
 import classNames from "classnames";
 
 const Header = () => {
+  const [isVisibleMenuButton, toggleMenuButton] = React.useState(false);
+
+  const menuRoutes = [
+    ["Episodes", "/", true],
+    ["Characters", "/characters"],
+    ["Quotes", "/quotes"],
+  ];
+
+  const menuItems = menuRoutes.map(([itemName, route, isExact], index) => {
+    return (
+      <li key={index} className={stl.menu__item}>
+        <NavLink to={route} exact={isExact} activeClassName={stl.menu__item_active}>
+          {itemName}
+        </NavLink>
+      </li>
+    );
+  });
+
+  const toggleMenu = () => toggleMenuButton((isVisibleMenuButton) => !isVisibleMenuButton);
+
   return (
-    <header className={classNames("header", `${stl.header}`)}>
+    <header className={stl.header}>
       <section className="container">
         <div className={stl.inner}>
           <h1 className={stl.title}>
             <span className={stl.title__part1}>Breaking</span>
             <span className={stl.title__part2}>Bad</span>
           </h1>
-          <nav className={stl.menu}>
-            <ul>
-              <li className={stl.menu__item}>
-                <NavLink to="/" exact activeClassName={stl.menu__item_active}>
-                  Episodes
-                </NavLink>
-              </li>
-              <li className={stl.menu__item}>
-                <NavLink to="/characters" activeClassName={stl.menu__item_active}>
-                  Characters
-                </NavLink>
-              </li>
-              <li className={stl.menu__item}>
-                <NavLink to="/quotes" activeClassName={stl.menu__item_active}>
-                  Quotes
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
+          <section className={stl.header__menu}>
+            <div className={classNames(stl.header__btn, { [`${stl.on}`]: isVisibleMenuButton })} onClick={toggleMenu}>
+              <div className={stl.one}></div>
+              <div className={stl.two}></div>
+              <div className={stl.three}></div>
+            </div>
+            <nav className={stl.menu}>
+              <ul>{menuItems}</ul>
+            </nav>
+          </section>
         </div>
       </section>
     </header>
