60a4f46c6628a38aa0700ae735214e888f1b4c06
Bugagashinka <voloshyn.andrew@gmail.com>
2020-07-29 09:24:48 +0300

    Fix gh-pages routing (404 page case)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Quotes from "pages/Quotes";
 import Episodes from "pages/Episodes";
 import Characters from "pages/Characters";
