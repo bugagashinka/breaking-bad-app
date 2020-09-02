127d4311cc203a850fee499ed74c2c3990d80e72
Bugagashinka <voloshyn.andrew@gmail.com>
2020-09-02 13:31:19 +0300

    Restore scroll position on the Quotes page after load more

--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -4,14 +4,33 @@ import { BBService } from "services";
 import { compose } from "utils";
 import { withData, withGeneralLayout, withLoadMore } from "hoc";
 
-const Quotes = ({ data: quotes }) => {
-  return (
-    <>
-      {quotes.map(({ id, ...data }) => (
-        <QuoteItem key={id} {...data} />
-      ))}
-    </>
-  );
-};
+class Quotes extends React.Component {
+  render() {
+    const quotes = this.props.data;
+    return (
+      <>
+        {quotes.map(({ id, ...data }) => (
+          <QuoteItem key={id} {...data} />
+        ))}
+      </>
+    );
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    debugger;
+    if (this.props.data.length > prevProps.data.length) {
+      const htmlElement = document.documentElement;
+      return htmlElement.scrollTop;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (snapshot) {
+      const htmlElement = document.documentElement;
+      htmlElement.scrollTop = snapshot;
+    }
+  }
+}
 
 export default compose(withData(BBService.getAllQuotes), withGeneralLayout({ isGrid: false }), withLoadMore)(Quotes);
